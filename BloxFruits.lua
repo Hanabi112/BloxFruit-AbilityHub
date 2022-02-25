4a9146e8589a7c5dc83e7f1b33e727e933cb7cf1
N0Fee1 <98322667+N0Fee1@users.noreply.github.com>
2022-02-25 12:34:39 +0700

    Update BloxFruits.lua

--- BloxFruits.lua
+++ BloxFruits.lua
@@ -1,3 +1,11 @@
+game:GetService("StarterGui"):SetCore("SendNotification",
+							{
+								Title = "Ability Hub",
+								Text = "Ability Hub Working Blox Fruits...",
+								Icon = "",
+							}
+						)
+						wait(1)
 _, Protected_by_MoonSecV2, Discord = 'discord.gg/gQEH2uZxUk'
 
 
